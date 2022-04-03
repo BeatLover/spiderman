e28d4367a5db429efc23475ebd75650effd1421f
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-04-03 17:25:18 -0400

    Update player.js

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent("player", {
   schema: {
     mass: { type: "number", default: 70 }, // kg
     bounce: { type: "number", default: 0.100000000000000000000 }, // 0 - 1 (larger is more bouncy)
-    gravity: { type: "number", default: -1.8 }, // m / s^2
+    gravity: { type: "number", default: -9.8 }, // m / s^2
     padding: { type: "number", default: 0.4 }, // m
     paddingHand: { type: "number", default: 0.03 }, // m
     handStrength: { type: "number", default: 100 },
